c94ad4fc2d4c67b9cab0c3b56428e15723322063
arunmithra <160711671+arunmithra@users.noreply.github.com>
2024-05-25 17:50:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u arundockerhub2024 -p $DOCKER_PASSWORD
-    docker tag test arundockerhub2024/dev
+    docker tag arun-cap arundockerhub2024/dev
     docker push arundockerhub2024/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
