ed135be2b60cc3b7e40d7584096b1a96181d19bc
arunmithra <160711671+arunmithra@users.noreply.github.com>
2024-05-28 23:19:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
 
 DOCKER_USERNAME="arundockerhub2024"
 DOCKER_PASSWORD="dckr_pat_VSckYH1eVrxbzFBqN0dxbSozN2E"
@@ -21,21 +20,3 @@ docker push "$DOCKER_USERNAME/${DOCKER_REPO}:${DOCKER_TAG}"
 echo "Deployment to Docker Hub completed successfully."
 
 docker-compose up -d
-
-=======
-if [[ $GIT_BRANCH == "origin/dev" ]]; then
-    # Build your project
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u arundockerhub2024 -p $DOCKER_PASSWORD
-    docker tag arun-cap arundockerhub2024/dev
-    docker push arundockerhub2024/dev
-
-elif [[ $GIT_BRANCH == "origin/main" ]]; then
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u arundockerhub2024 -p $DOCKER_PASSWORD
-    docker tag arun-cap arundockerhub2024/prod 
-    docker push arundockerhub2024/prod
-fi
->>>>>>> origin/dev
